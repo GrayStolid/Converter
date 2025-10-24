da9dbbc6e8c7a0036f1a2ad71e3d77ebbb93011f
Raoyat <slipknotstolid@gmail.com>
2025-10-24 11:26:14 +0330

    Add flutter project
    contain multiple local generated files

---
+++ flutter_app/android/app/build/intermediates/cxx/Debug/1j33in6h/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,20 @@
+@echo off
+"E:\\AndroidStudioSDK\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\Stolid\\flutter\\3.29.2\\packages\\flutter_tools\\gradle\\src\\main\\groovy" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=24" ^
+  "-DANDROID_PLATFORM=android-24" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=E:\\AndroidStudioSDK\\ndk\\27.0.12077973" ^
+  "-DCMAKE_ANDROID_NDK=E:\\AndroidStudioSDK\\ndk\\27.0.12077973" ^
+  "-DCMAKE_TOOLCHAIN_FILE=E:\\AndroidStudioSDK\\ndk\\27.0.12077973\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=E:\\AndroidStudioSDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=F:\\Projects\\Python\\Learning\\Flet\\SendCoordinate\\build\\flutter\\android\\app\\build\\intermediates\\cxx\\Debug\\1j33in6h\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=F:\\Projects\\Python\\Learning\\Flet\\SendCoordinate\\build\\flutter\\android\\app\\build\\intermediates\\cxx\\Debug\\1j33in6h\\obj\\arm64-v8a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BF:\\Projects\\Python\\Learning\\Flet\\SendCoordinate\\build\\flutter\\android\\app\\.cxx\\Debug\\1j33in6h\\arm64-v8a" ^
+  -GNinja ^
+  -Wno-dev ^
+  --no-warn-unused-cli
---
+++ flutter_app/android/app/src/main/kotlin/com/mycompany/sendcoordinate/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.sendcoordinate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
